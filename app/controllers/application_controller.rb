d4992112b52c4cf6648fda5a9023787488f6a5cd
tangent500 <tangent500@yahoo.co.jp>
2020-06-28 01:46:15 +0000

    follow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_diaryposts = user.diaryposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
